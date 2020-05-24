d586b084d01b8c91b6354751889ac2229a592c18
jackaroo <friendss8@msn.com>
2020-05-24 10:00:54 +0100

    Added CORS Support to lambda http server

--- src/lambda/HttpServer.js
+++ src/lambda/HttpServer.js
@@ -19,6 +19,37 @@ export default class HttpServer {
     }
 
     this.#server = new Server(serverOptions)
+
+    // Enable CORS preflight response
+    this.#server.ext('onPreResponse', (request, h) => {
+      if (request.headers.origin) {
+        const response = request.response.isBoom
+          ? request.response.output
+          : request.response
+
+        response.headers['access-control-allow-origin'] = request.headers.origin
+        response.headers['access-control-allow-credentials'] = 'true'
+
+        if (request.method === 'options') {
+          response.statusCode = 200
+          response.headers['access-control-expose-headers'] =
+            'content-type, content-length, etag'
+          response.headers['access-control-max-age'] = 60 * 10
+
+          if (request.headers['access-control-request-headers']) {
+            response.headers['access-control-allow-headers'] =
+              request.headers['access-control-request-headers']
+          }
+
+          if (request.headers['access-control-request-method']) {
+            response.headers['access-control-allow-methods'] =
+              request.headers['access-control-request-method']
+          }
+        }
+      }
+
+      return h.continue
+    })
   }
 
   async start() {
